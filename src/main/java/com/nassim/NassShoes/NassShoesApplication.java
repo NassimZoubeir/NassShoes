7086eabc55295aad7a3c9d2dbec0836d1d23396d
Nassim <nassimz@outlook.fr>
2025-04-14 16:09:33 +0200

    first commit

---
+++ src/main/java/com/nassim/NassShoes/NassShoesApplication.java
@@ -0,0 +1,13 @@
+package com.nassim.NassShoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NassShoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NassShoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nassim/NassShoes/NassShoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nassim.NassShoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NassShoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
